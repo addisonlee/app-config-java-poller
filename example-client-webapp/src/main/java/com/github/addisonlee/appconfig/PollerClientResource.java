f03d3badb3f1938769ac51513898c9ed827a95f7
Addison Lee <alee@thoughtworks.com>
2012-12-20 23:07:57 -0500

    resurrecting the piano demo

--- example-client-webapp/src/main/java/com/github/addisonlee/appconfig/PollerClientResource.java
+++ example-client-webapp/src/main/java/com/github/addisonlee/appconfig/PollerClientResource.java
@@ -1,15 +1,51 @@
 package com.github.addisonlee.appconfig;
 
+import javax.sound.midi.Instrument;
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.ShortMessage;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import java.util.ArrayList;
+import java.util.List;
 
-@Path("/poller")
+@Path("/midi")
 public class PollerClientResource {
-    @GET
-    @Path("/md5")
+    @POST
+    @Path("/send")
+    @Consumes("application/json")
     @Produces("application/json")
-    public String md5() {
-        return PollerServletLifeCycleListener.getHash();
+    public JsonMidiMessage send(JsonMidiMessage json) throws InvalidMidiDataException, MidiUnavailableException {
+        ShortMessage midi = new ShortMessage();
+        midi.setMessage(json.command, json.channel, json.note, json.velocity);
+        Application.getSynth().getReceiver().send(midi, -1);
+        return json;
+    }
+
+    @GET
+    @Path("/list")
+    @Produces("application/json")
+    public InstrumentData[] list() throws InvalidMidiDataException, MidiUnavailableException {
+        List<InstrumentData> instrumentsList = new ArrayList<>();
+        for (Instrument instrument : Application.getSynth().getAvailableInstruments()) {
+            InstrumentData instrumentData = new InstrumentData();
+            instrumentData.name = instrument.getName();
+            instrumentsList.add(instrumentData);
+        }
+        return instrumentsList.toArray(new InstrumentData[instrumentsList.size()]);
+    }
+
+    public static class JsonMidiMessage {
+        public int command;
+        public int channel;
+        public int note;
+        public int velocity;
+    }
+
+    public static class InstrumentData {
+        public String name;
     }
 }
